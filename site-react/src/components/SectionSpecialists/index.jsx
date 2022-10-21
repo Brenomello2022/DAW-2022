6aa597c0039de4a28f22b5b884f2653bbb75175f
Brenomello2022 <brenodemello2022@gmail.com>
2022-10-20 23:23:29 -0300

    Aula 20/10/2022

---
+++ site-react/src/components/SectionSpecialists/index.jsx
@@ -0,0 +1,54 @@
+import './style.css';
+
+export function SectionSpecialists() {
+    return (
+        <section id="specialists">
+
+            <h2>Converse com um Especialista</h2>
+
+            <div className="specialists-content">
+
+                <div className="specialist">
+
+                    <h5>Tutu Williams</h5>
+
+                    <p>London</p>
+
+                    <p className="specialists-detail">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Perspiciatis alias officiis harum accusantium, reiciendis voluptate,
+                    quod cupiditate provident vero consequatur aut nulla veniam id atque et quasi,
+                    necessitatibus ducimus iure.</p>
+
+                </div>
+
+                <div className="specialist">
+
+                    <h5>Mark Zion</h5>
+
+                    <p>New York City</p>
+
+                    <p className="specialists-detail">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Perspiciatis alias officiis harum accusantium, reiciendis voluptate,
+                    quod cupiditate provident vero consequatur aut nulla veniam id atque et quasi,
+                    necessitatibus ducimus iure.</p>
+
+                </div>
+
+                <div className="specialist">
+
+                    <h5>Essien Crest</h5>
+
+                    <p>Berlin</p>
+
+                    <p className="specialists-detail">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Perspiciatis alias officiis harum accusantium, reiciendis voluptate,
+                    quod cupiditate provident vero consequatur aut nulla veniam id atque et quasi,
+                    necessitatibus ducimus iure.</p>
+
+                </div>
+
+            </div>
+
+        </section>
+    )
+}
---
+++ site-react/src/components/SectionValues/index.jsx
@@ -0,0 +1,53 @@
+import './style.css';
+
+import imgLogo from '../../assets/img/place-company.png'
+
+export function SectionValues() {
+    return (
+        <section id="values">
+
+            <h2>Quem é a Imobiliária Unimater ?</h2>
+
+            <div className="values-container">
+
+                <img src="./assets/img/place-company.png" alt="Foto da Firma" />
+
+                <div className="values-content">
+
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                    Commodi, quidem. Corrupti debitis repellat impedit perspiciatis incidunt. 
+                    Optio ullam neque ipsum repudiandae temporibus cum! Facere, in vitae?
+                    Sit, necessitatibus? Nihil, minima.</p>
+            
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                    Incidunt iure eveniet deleniti veritatis cupiditate nesciunt illum! 
+                    Velit numquam nemo accusamus recusandae vitae nulla dolorum, 
+                    distinctio error dolore nisi, culpa iusto!</p>
+
+                    <div className="value-detail">
+
+                        <div className="value">
+
+                            <span>Alto Padrão</span>
+
+                            <span>Agilidade</span>
+
+                        </div>
+
+                        <div className="value">
+
+                            <span>Facilidade</span>
+                        
+                            <span>Confiabilidade</span>
+                        
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </section>
+    )
+}
